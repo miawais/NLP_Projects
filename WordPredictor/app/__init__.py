9d83a4eb7e1a75504181adfa523c193351919872
miawais <awaisbinmukhtar@gmail.com>
2024-06-03 18:36:05 +0500

    new file structure for Complete Project

--- WordPredictor/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__, template_folder='D:/NLP_Projects/WordPredictor/templates')
-    with app.app_context():
-        from . import routes
-        return app
